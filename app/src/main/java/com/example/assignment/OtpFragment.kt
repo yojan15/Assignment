8a8be94c497fdad906da450fdc99e2599b46ff84
yojan khuble <85030838+yojan15@users.noreply.github.com>
2024-03-07 14:53:23 +0530

    first commit

---
+++ app/src/main/java/com/example/assignment/OtpFragment.kt
@@ -0,0 +1,21 @@
+package com.example.assignment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.assignment.databinding.FragmentOtpBinding
+
+class OtpFragment : Fragment() {
+    private lateinit var binding : FragmentOtpBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentOtpBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+}
